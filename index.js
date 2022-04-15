5a826c2f3a294122f86719c757a91cfba1c397d0
Cindy Germain <cindymarguerite@gmail.com>
2022-04-15 05:45:10 -0600

    commit tree f866011f72d0ee26810b6f59175036523101e915

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(string) {
+  const newString = string.split("").reverse().join("")
+
+  if(string === newString){
+    return true
+  } else {
+    return false
+  }
+
+
 }
 
 /* 
-  Add your pseudocode here
+  declare function
+  set argument to a string
+  reverse string (need to convert to array to use .reverse)
+  compare string to new string
+  return true/false
 */
 
 /*
-  Add written explanation of your solution here
+  i wanted to use .reverse function, but it only works on arrays
+  -need to split string into individ elements (into an array)
+  -then reverse those individ elements
+  -then join individ elements/array back into a string
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+
+  console.log(isPalindrome("mom"))
+  
+  console.log(isPalindrome("dog)"))
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
